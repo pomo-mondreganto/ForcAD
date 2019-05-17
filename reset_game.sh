5a7d228e0ac151d1b0c535c15022eab423a3b35c
pomo-mondreganto <1728390Roman>
2019-05-17 10:45:26 +0300

    Reset game now cds to right dir & runs docker-compose down

--- reset_game.sh
+++ reset_game.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+cd -P -- "$(dirname -- "$0")" && pwd -P
+
 rm -rf docker_volumes/postgres/data
 rm -f docker_volumes/shared/initialized
 rm -f docker_volumes/celery/celery*
 rm -f docker_volumes/shared/game_running
 rm -f docker_volumes/shared/round
+docker-compose down -v --remove-orphans
