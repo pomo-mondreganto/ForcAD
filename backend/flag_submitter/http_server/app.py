b67043189828e7356a438c906d41d94b7e8556a0
pomo-mondreganto <1728390Roman>
2019-05-18 13:46:28 +0300

    Fixed http submitter

--- backend/flag_submitter/http_server/app.py
+++ backend/flag_submitter/http_server/app.py
@@ -13,42 +13,18 @@ import helpers
 from helpers import exceptions
 
 
-class ReverseProxied(object):
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        return self.app(environ, start_response)
-
-
 app = Flask(__name__)
 app.config['APPLICATION_ROOT'] = '/flags'
-app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 
 def get_response(status_code, data=None, error=None):
     if error:
-        return make_response({'error': error}, status_code)
+        return make_response(json.dumps({'error': error}), status_code)
 
     return make_response(json.dumps(data), status_code)
 
 
-@app.route('/status')
-def status():
-    return 'OK'
-
-
-@app.route('/submit')
+@app.route('/flags/', methods=['POST'])
 def submit():
     team_token = request.headers.get('X-TeamToken')
     team_id = storage.teams.get_team_id_by_token(team_token)
@@ -59,7 +35,7 @@ def submit():
         return get_response(error='Game is not available yet', status_code=403)
 
     if not request.is_json or not isinstance(request.json.get('flags'), list):
-        return get_response(error='Submit your flags as json in array with key "flags', status_code=400)
+        return get_response(error='POST your flags as json in array with key "flags', status_code=400)
 
     responses = []
     for flag_str in request.json['flags']:
