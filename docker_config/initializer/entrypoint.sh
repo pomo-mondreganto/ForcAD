dd316a1e409b498469e7a26bb203406078f779a1
Roman Nikitin <nikrom2012@me.com>
2020-02-05 01:13:20 +0300

    Style factor

--- docker_config/initializer/entrypoint.sh
+++ docker_config/initializer/entrypoint.sh
@@ -3,21 +3,21 @@
 set +e
 echo "[*] Checking is postgres container started"
 python3 /db_check.py
-while [[ $? != 0 ]] ; do
-    sleep 3; echo "[*] Waiting for postgres container..."
-    python3 /db_check.py
+while [[ $? != 0 ]]; do
+  sleep 3
+  echo "[*] Waiting for postgres container..."
+  python3 /db_check.py
 done
 
 python3 /check_initialized.py
-if [[ $? == 0 ]]
-then
-    echo "[+] Already initialized, starting"
+if [[ $? == 0 ]]; then
+  echo "[+] Already initialized, starting"
 else
-    echo "[*] Initializing game"
-    set -e
+  echo "[*] Initializing game"
+  set -e
 
-    echo "[*] Resetting & initializing database"
-    python3 /app/scripts/full_reset.py
+  echo "[*] Resetting & initializing database"
+  python3 /app/scripts/full_reset.py
 
-    echo "[+] Successfully initialized"
+  echo "[+] Successfully initialized"
 fi
