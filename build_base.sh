ccf3f1d9e6ae930fbfe488ef68dbfffc7fe76316
Roman Nikitin <nikrom2012@me.com>
2020-02-15 12:32:46 +0300

    Updated build_base.sh

--- build_base.sh
+++ build_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "[*] Building backend base"
 docker build --squash --no-cache -t pomomondreganto/forcad_base:latest -f docker_config/base_images/backend.Dockerfile "${DIR}"
