81a787f4c52e83a40a9221258df9cee929d24aca
Roman Nikitin <nikrom2012@me.com>
2020-02-15 12:32:15 +0300

    Made builds fast (added base image for postgres) Removed old rating system Renamed nginx/app.conf to default.conf to prevent bugs on older dockers (???)

---
+++ build_base.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "[*] Building backend base"
+docker build --squash --no-cache -t pomomondreganto/forcad_base:latest -f docker_config/base_images/backend.Dockerfile "${DIR}"
+
+echo "[*] Building postgres base"
+docker build --squash --no-cache -t pomomondreganto/forcad_postgres:latest -f docker_config/base_images/postgres.Dockerfile "${DIR}"
+
+echo "[*] Pushing backend base"
+docker push pomomondreganto/forcad_base:latest
+
+echo "[*] Pushing postgres base"
+docker push pomomondreganto/forcad_postgres:latest
+
+echo "[+] Done!"
