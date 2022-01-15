03bf9b60c217c97fee16c5ae6eaece68be53fc17
Roman Nikitin <nikrom2012@me.com>
2022-01-15 19:26:38 +0300

    Backward compatibility with docker-compose

--- tests/wait_for_start.py
+++ tests/wait_for_start.py
@@ -13,9 +13,17 @@ from helpers import wait_rounds
 
 DOCKER_COMPOSE_FILE = 'docker-compose-tests.yml'
 INITIALIZER_CONTAINER_NAME = 'forcad-initializer-1'
+INITIALIZER_CONTAINER_NAME_OLD = 'forcad_initializer_1'
 
 
-def wait_for_container(name):
+def wait_for_initializer():
+    # Docker Compose broke compatibility in container naming.
+    out = subprocess.check_output(['docker', 'ps', '-a']).decode()
+    if INITIALIZER_CONTAINER_NAME in out:
+        name = INITIALIZER_CONTAINER_NAME
+    else:
+        name = INITIALIZER_CONTAINER_NAME_OLD
+
     print(f'Waiting for container {name}')
     command = ['docker', 'wait', name]
     result = int(subprocess.check_output(command).decode().strip())
@@ -27,7 +35,7 @@ def wait_for_container(name):
 
 
 def wait_all():
-    wait_for_container(INITIALIZER_CONTAINER_NAME)
+    wait_for_initializer()
     wait_rounds(3)
 
 
