9bd5d239b89b02b554c20fe4321802feddb674fd
Roman Nikitin <nikrom2012@me.com>
2022-11-27 23:00:54 +0300

    Better clean, tests from source image, upgrade reqs, fix shared pg conn

--- scripts/release_base.sh
+++ scripts/release_base.sh
@@ -7,8 +7,8 @@ pushd "${BASE_DIR}" >/dev/null
 
 IMAGE="ghcr.io/pomo-mondreganto/forcad_base:${VERSION}"
 
-echo "[*] Building ${IMAGE}"
-docker buildx build --platform linux/amd64,linux/arm64 -t "${IMAGE}" -f docker_config/base_images/backend.Dockerfile "$*" "${BASE_DIR}"
+echo "[*] Building ${IMAGE}, base dir ${BASE_DIR}"
+docker buildx build --platform linux/amd64,linux/arm64 -t "${IMAGE}" -f docker_config/base_images/backend.Dockerfile "$@" "${BASE_DIR}"
 
 popd >/dev/null
 
